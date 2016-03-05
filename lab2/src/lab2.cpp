b03db1ef390823370032287f655c396f7726bc03
awanishanand <awnd13is@cmrit.ac.in>
2016-03-05 14:03:15 +0530

    Update lab2.cpp

--- lab2/src/lab2.cpp
+++ lab2/src/lab2.cpp
@@ -1,5 +1,5 @@
 //============================================================================
-// Name        : lab2.cpp
+// Name        : fslab.cpp
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
@@ -7,9 +7,64 @@
 //============================================================================
 
 #include <iostream>
+#include<fstream>
+#include<sstream>
 using namespace std;
+class student{
+	int semester;
+	string name;
+	string usn;
+	string branch;
+public:
+	string buffer;
+	void read()
+	{
+		cout<<"enter name";
+		cin>>name;
+		cout<<"enter usn";
+		cin>>usn;
+		cout<<"enter sem";
+		cin>>semester;
+		cout<<"enter branch";
+		cin>>branch;
+	}
+	void pack()
+	{
+		string str1,temp;
+		stringstream out;
+		out<<semester;
+		str1=out.str();
 
+		string buffer;
+		temp+=usn+'|'+name+'|'+branch+'|'+str1;
+		for(int i=buffer.length();i<100;i++)
+			temp+='$';
+			buffer+=temp;
+		cout<<buffer;
+		fstream fp;
+		fp.open("data1.txt",ios::out|ios::app);
+		fp<<buffer;
+		fp.close();
+	}
+	void write()
+	{
+		cout<<"\n from write";
+	}
+};
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	student ob;
+	int choice;
+	cout<<"1.insert 2.search 3.delete 4.modification"<<endl;
+	cout<<"choice";
+	cin>>choice;
+	switch(choice)
+	{
+	case 1:
+		   ob.read();
+		   ob.pack();
+		   ob.write();
+		   break;
+	}
+
 	return 0;
 }
